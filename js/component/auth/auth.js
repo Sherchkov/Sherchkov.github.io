453046bacc52de90fda10bea593a9e0ecc0fed23
timursaet <s.timursaet@yandex.ru>
2020-04-21 15:49:50 +0500

    add icons for formAuthorization

--- js/component/auth/auth.js
+++ js/component/auth/auth.js
@@ -1,16 +1,16 @@
 define(["base/component", "base/helpers", "css!component/auth/style.css"], function (Component) {
     class auth extends Component {
-      render() {
-          return `<div class="auth">
+        render() {
+            return `<div class="auth">
                 <div class="auth__main">Авторизация</div>
                 <div class="auth__login">
                     <form>
                         <p>
-                            <img src="img/login.jpg" class="auth__login_view">
+                            <img src="img/icons/system/auth/login.jpg" class="auth__login_view">
                             <input type="text" size="20" placeholder="login" name="login" pattern="^[a-zA-Z]+$">
                         </p>
                         <p>
-                            <img src="img/block.jpg" class="auth__password_view">
+                            <img src="img/icons/system/auth/block.jpg" class="auth__password_view">
                             <input type="password" size="20" name="password" placeholder="password">
                         </p>
                     </form>
@@ -20,7 +20,7 @@ define(["base/component", "base/helpers", "css!component/auth/style.css"], funct
                     <button>Зарегистрироваться</button>
                 </div>
             </div>`;
-      }
+        }
     }
     return auth;
 });
