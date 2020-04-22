b77117f7a9faaaa87d872325fd52e17dc49ba2cb
timursaet <s.timursaet@yandex.ru>
2020-04-22 12:12:10 +0500

    add security forms authorizations

--- js/component/auth/auth.js
+++ js/component/auth/auth.js
@@ -34,45 +34,57 @@ define(['base/component', 'css!component/auth/style.css'], function (Component) 
         }
 
         sign() {
-            const urlencoded = new URLSearchParams();
-
-            urlencoded.append("login", document.forms["authorization"].elements["login"].value);
-            urlencoded.append("password", document.forms["authorization"].elements["password"].value);
-
             this._success = this.getContainer().querySelector('.auth__success');
 
-            const requestOptions = {
-                method: 'POST',
-                headers: {"Content-Type": "application/x-www-form-urlencoded"},
-                body: urlencoded,
-                "credentials": "include"
-            };
+            if (document.forms["authorization"].elements["login"].value == '' ||
+                document.forms["authorization"].elements["password"].value == '') {
+                this._success.innerHTML = 'Введенные поля пустые!';
+                this._success.style.display = 'block';
+            } else {
+                const urlencoded = new URLSearchParams();
 
-            fetch("https://tensor-school.herokuapp.com/user/login", requestOptions)
-                .then(response => response.text())
-                .then(result => {
-                    if (result == "Unauthorized") {
-                        this._success.innerHTML = 'Пользователь не зарегистрирован!';
-                        this._success.style.display = 'block';
-                    } else {
-                        this.unmount();
-                        if ( window.innerWidth > 800 ) {
-                            require(["page/profile"], function(Profile){
-                                const profile = factory.create(Profile, {});
-                                profile.mount(document.body);
-                            });
-                        }else{
-                            require(["page/ProfileMobile"], function(profileMobile){
-                                const profile = factory.create(profileMobile, {});
-                                profile.mount(document.body);
-                            });
+                urlencoded.append("login", document.forms["authorization"].elements["login"].value);
+                urlencoded.append("password", document.forms["authorization"].elements["password"].value);
+
+                const requestOptions = {
+                    method: 'POST',
+                    headers: {"Content-Type": "application/x-www-form-urlencoded"},
+                    body: urlencoded,
+                    "credentials": "include"
+                };
+
+                fetch("https://tensor-school.herokuapp.com/user/login", requestOptions)
+                    .then(response => response.text())
+                    .then(result => {
+                        if (result == "Unauthorized") {
+                            this._success.innerHTML = 'Пользователь не зарегистрирован!';
+                            this._success.style.display = 'block';
+                        } else {
+                            this.unmount();
+                            if (window.innerWidth > 800) {
+                                require(["page/profile"], function (Profile) {
+                                    const profile = factory.create(Profile, {});
+                                    profile.mount(document.body);
+                                });
+                            } else {
+                                require(["page/ProfileMobile"], function (profileMobile) {
+                                    const profile = factory.create(profileMobile, {});
+                                    profile.mount(document.body);
+                                });
+                            }
                         }
-                    }
-                })
-                .catch(error => console.log('error', error));
+                    })
+                    .catch(error => console.log('error', error));
+            }
         }
-
         registerPerson() {
+            this._success = this.getContainer().querySelector('.auth__success');
+
+            if (document.forms["authorization"].elements["login"].value == '' ||
+                document.forms["authorization"].elements["password"].value == '') {
+                this._success.innerHTML = 'Введенные поля пустые!';
+                this._success.style.display = 'block';
+            } else {
             const urlencoded = new URLSearchParams();
 
             urlencoded.append("login", document.forms["authorization"].elements["login"].value);
@@ -98,7 +110,7 @@ define(['base/component', 'css!component/auth/style.css'], function (Component) 
                     }
                 })
                 .catch(error => console.log('error', error));
-        }
+        }}
     }
     return Auth;
 });
