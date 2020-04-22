43c9a02677adab7a96e6f4a5b101600839b81b51
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-04-22 15:12:27 +0500

    Галлерея
    Дополнил галерею 
    Добавил модальное окно галереи, слайдера, вызова из модального окна галерее, модальное окно слайдера, ну и т.д.

--- js/component/gallery/gallery.js
+++ js/component/gallery/gallery.js
@@ -1,21 +1,27 @@
-define(['base/component', 'component/action/slider', 'modal/ActionModal', 'modal/ModalSlider', 'server/json', 'css!component/gallery/gallery.min'], function (Component, slider, ActionModal, ModalSlider, json) {
+define(['base/component', 'component/action/slider', 'server/json', 'css!component/gallery/gallery'], function (Component, slider, json) {
 	'use strict';
 
-	let items = json.gallery.images;
 
 	class Gallery extends Component {
 
 	    render() {
-	        let items = json.gallery.images;
 	        let sliderContainer = ''
-	        for (let i = 0; i < items.length; i++){
+	        this.items = json.gallery.images;
+	        for (let i = 0; i < this.items.length; i++){
+	        	if (i == 4) {
+	        		break;
+	        	}
 	            sliderContainer += `
-	                <div class="slider-container__item slider-container__item_four">
-	                  <img src="${items[i]}" alt="фотография" class="slider-container__img slider-container__img_min">
+	                <div class="slider-container__item slider-container__item_gallery" data-position="${i}">
+	                  <img src="${this.items[i]}" alt="фотография" class="slider-container__img slider-container__img_min">
 	                </div>`;
 	        }
 	        return `
 	            <div class="content-gallery content_default">
+	            	<div class="content-gallery__head">
+						<span class="content-gallery__title">Мои фотографии</span>
+						<button class="content-gallery__add">добавить</button>
+	            	</div>
 	                <div class="slider slider-mini">
 	                  <div class="slider-arrowLeft sliderLeft" title="Назад">
 	                    <svg class="slider-arrowLeft__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve"><polygon points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153 "/></svg>
@@ -32,48 +38,75 @@ define(['base/component', 'component/action/slider', 'modal/ActionModal', 'modal
 	    }
 
 	    afterMount(){
-	        slider(document.getElementById(this.id));
-	        this.subscribeTo(this.getContainer(), 'click', this.createBigSlider.bind(this));
+	        slider(
+	        	document.getElementById(this.id),
+	        	{
+	        		items : this.items,
+	        		classItem : 'slider-container__item_gallery',
+	        		classImg : 'slider-container__img_min'
+	        	}
+	        );
+	        this.subscribeTo(this.getContainer(), 'click', this.chooseAction.bind(this));
 	    }
 
-	    createBigSlider(event){
-	        if (event.target.classList.contains('slider-container__img')) {
-	            let element = event.target.parentElement,
-	                position = 0,
-	                items = [];
-
-	            /*console.log(element.previousSibling)
-	            return;*/
-	            // заносим левые элементы в список items
-	            while (element.previousSibling) {
-
-	                element = element.previousSibling;
-	                if ( element.nodeType === 1) {
-	                    items.push( element.querySelector('.slider-container__img').getAttribute('src') );
-	                }   
-	            }
-
-	            position = items.length;
-	            element = event.target.parentElement;
-	            items.push( element.querySelector('.slider-container__img').getAttribute('src') );
-
-	            // заносим правые элементы в список items
-	            while (element.nextSibling) {
-	                element = element.nextSibling;
-	                if ( element.nodeType === 1) {
-	                    items.push( element.querySelector('.slider-container__img').getAttribute('src') );
-	                }   
-	            }
-
-	            new ActionModal({
-	                children : ModalSlider,
-	                position : position,
-	                items : items,
-	            });
-
-	        }
+	    chooseAction(event){
+	    	if (event.target.classList.contains('content-gallery__title')) {
+	    		this.createBigGallery();
+	    	}else if (event.target.classList.contains('content-gallery__add')) {
+	    		this.addPhoto();
+	    	}else if (event.target.classList.contains('slider-container__img')) {
+	    		this.createBigSlider(event.target.parentElement);
+	    	}
 	    }
 
+	    createBigGallery(){
+	    	let items = this.items;
+	    	require(['modal/ActionModal', 'modal/ModalGallery'], function(ActionModal, ModalGallery){
+	    		new ActionModal({
+	    		    children : ModalGallery,
+	    		    theme: 'white',
+	    		    items : items,
+	    		});  
+	    	})
+
+	    }
+
+	    createBigSlider(element){
+	    	let position = 0,
+	    		items = this.items;
+
+	    	//узнаем позицию нажатой картинки
+	    	while (element.previousSibling) {
+	    	    element = element.previousSibling;
+	    	    if ( element.nodeType === 1) {
+	    	        position++;
+	    	    }   
+	    	}
+
+	    	require(['modal/ModalSlider'], function(ModalSlider){
+
+	    		if (  typeof(modalslider) !== "undefined" ) {
+	    		    modalslider.unmount();
+	    		}
+
+	    		modalslider = factory.create(ModalSlider, {
+	    			position : position,
+	    			items : items
+	    		});
+	    		modalslider.mount(document.body);
+	    	})
+	    }
+
+	    addPhoto(){
+	    	require(['modal/ModalAddPhoto'], function(ModalSlider){
+	    		if (  typeof(modalAddPhoto) !== "undefined" ) {
+	    		    modalAddPhoto.unmount();
+	    		}
+
+	    		modalAddPhoto = factory.create(ModalSlider, {});
+	    		modalAddPhoto.mount(document.body);
+	    	})
+	    }
 	}
 
 
