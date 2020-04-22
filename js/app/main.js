de0478bbc9578985af277b5086912be8306a81b8
timursaet <s.timursaet@yandex.ru>
2020-04-22 11:51:57 +0500

    Authorization success

--- js/app/main.js
+++ js/app/main.js
@@ -1,16 +1,16 @@
-if ( window.innerWidth > 800 ) {
-	define(["page/profile"], function(Profile){
-		const profile = factory.create(Profile, {});
-	    profile.mount(document.body);
-	});
-}else{
-	define(["page/ProfileMobile"], function(profileMobile){
-		const profile = factory.create(profileMobile, {});
-	    profile.mount(document.body);
-	});
-}
+// if ( window.innerWidth > 800 ) {
+// 	define(["page/profile"], function(Profile){
+// 		const profile = factory.create(Profile, {});
+// 	    profile.mount(document.body);
+// 	});
+// }else{
+// 	define(["page/ProfileMobile"], function(profileMobile){
+// 		const profile = factory.create(profileMobile, {});
+// 	    profile.mount(document.body);
+// 	});
+// }
 
-// define(["page/Authorization"], function(authorization){
-// 	const profile = factory.create(authorization, {});
-// 	profile.mount(document.body);
-// });
+define(["page/Authorization"], function(authorization){
+	const profile = factory.create(authorization, {});
+	profile.mount(document.body);
+});
--- js/component/auth/auth.js
+++ js/component/auth/auth.js
@@ -39,6 +39,8 @@ define(['base/component', 'css!component/auth/style.css'], function (Component) 
             urlencoded.append("login", document.forms["authorization"].elements["login"].value);
             urlencoded.append("password", document.forms["authorization"].elements["password"].value);
 
+            this._success = this.getContainer().querySelector('.auth__success');
+
             const requestOptions = {
                 method: 'POST',
                 headers: {"Content-Type": "application/x-www-form-urlencoded"},
@@ -47,8 +49,25 @@ define(['base/component', 'css!component/auth/style.css'], function (Component) 
             };
 
             fetch("https://tensor-school.herokuapp.com/user/login", requestOptions)
-                .then(response => {
-                    console.log(response);
+                .then(response => response.text())
+                .then(result => {
+                    if (result == "Unauthorized") {
+                        this._success.innerHTML = 'Пользователь не зарегистрирован!';
+                        this._success.style.display = 'block';
+                    } else {
+                        this.unmount();
+                        if ( window.innerWidth > 800 ) {
+                            require(["page/profile"], function(Profile){
+                                const profile = factory.create(Profile, {});
+                                profile.mount(document.body);
+                            });
+                        }else{
+                            require(["page/ProfileMobile"], function(profileMobile){
+                                const profile = factory.create(profileMobile, {});
+                                profile.mount(document.body);
+                            });
+                        }
+                    }
                 })
                 .catch(error => console.log('error', error));
         }
@@ -69,10 +88,14 @@ define(['base/component', 'css!component/auth/style.css'], function (Component) 
             };
 
             fetch("https://tensor-school.herokuapp.com/user/create", requestOptions)
-                .then(response => response.text())
-                .then(result => {
-                        this._success.innerHTML += 'Пользователь успешно зарегистрирован!';
+                .then(response => {
+                    if (response.status == '400') {
+                        this._success.innerHTML = 'Пользователь уже зарегистрирован!';
                         this._success.style.display = 'block';
+                    } else {
+                        this._success.innerHTML = 'Пользователь успешно зарегистрирован!';
+                        this._success.style.display = 'block';
+                    }
                 })
                 .catch(error => console.log('error', error));
         }
