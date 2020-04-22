780a1307a16df7f2136344689fefed65da89d51f
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-04-22 15:15:14 +0500

    Изменил модальное окно галереи

--- js/component/modal/ModalSlider.js
+++ js/component/modal/ModalSlider.js
@@ -1,36 +1,93 @@
 define(['base/component', 'component/action/slider'], function (Component, slider) {
 	'use strict';
 
-	class ModalSlider extends Component{
+	class ModalSlider extends Component {
+	    
 	    render(options) {
-	        this.items = options.items;
+	    	console.log("options", options);
+
+	    	this.items = options.items;
 	        this.position = options.position;
-	        return ` 
-	            <div class="slider slider-max">
-	                <div class="slider-arrowLeft sliderLeft" title="Назад">
-	                  <svg class="slider-arrowLeft__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve"><polygon points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153 "/></svg>
-	                </div>
-	                <div class="slider-container">
-	                  <div class="slider-container__item slider-container__item_one" data-position="${options.position}">
-	                    <img src="${options.items[options.position]}" alt="фотография" class="slider-container__img slider-container__img_max">
-	                  </div>
-	                </div>
-	                <div class="slider-arrowRight sliderRight" title="Вперед">
-	                  <svg class="slider-arrowRight__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve"><polygon points="58.65,267.75 175.95,153 58.65,35.7 94.35,0 247.35,153 94.35,306"/></svg>
+
+	        return `
+	            <div class="modalSlidder">
+	                <button class="modal__close">
+	                  <svg class="modal__close_icon" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g><line class="cls-1" x1="7" x2="25" y1="7" y2="25"/><line class="cls-1" x1="7" x2="25" y1="25" y2="7"/></g></svg>
+	                </button>
+	                <div class="modal-content modal-content_dark">
+	                    <div class="slider slider-max">
+	                        <div class="slider-arrowLeft sliderLeft" title="Назад">
+	                          <svg class="slider-arrowLeft__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve"><polygon points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153 "/></svg>
+	                        </div>
+	                        <div class="slider-container">
+	                          <div class="slider-container__item slider-container__item_modal" data-position="${options.position}">
+	                            <img src="${options.items[options.position]}" alt="фотография" class="slider-container__img slider-container__img_max">
+	                          </div>
+	                        </div>
+	                        <div class="slider-arrowRight sliderRight" title="Вперед">
+	                          <svg class="slider-arrowRight__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve"><polygon points="58.65,267.75 175.95,153 58.65,35.7 94.35,0 247.35,153 94.35,306"/></svg>
+	                        </div>
+	                    </div>
 	                </div>
 	            </div>
 	        `;
 	    }
 
-	    afterMount(){
-	        slider(
-	            document.getElementById(this.id),
-	            {
-	                position: this.position,
-	                items: this.items,
-	            }
-	        );
+	    beforeMount(){
+
+			if ( document.querySelector('.modal') ) {
+				document.querySelector('.modal').style.opacity = 0;
+			}
+
+		}
+
+	    afterMount() {
+	    	if ( !document.querySelector('.modal') ) {
+				let html = document.getElementsByTagName('html')[0],
+					WidthBefore = html.clientWidth;
+
+				html.classList.add('html_overflow');
+				let WidthAfter = html.clientWidth;
+				html.style.marginRight = `${WidthAfter-WidthBefore}px`;	
+			}
+	    	
+	    	slider(
+	    	    document.getElementById(this.id),
+	    	    {
+	    	        position: this.position,
+	    	        items: this.items,
+	    	    }
+	    	);
+
+	        this._closeButton = this.getContainer().querySelector('.modal__close');
+	        this.subscribeTo(this._closeButton, 'click', this.onClose.bind(this));
+	        this.subscribeTo(this.getContainer(), 'click', this.onCloseModal.bind(this));
 	    }
+
+	    beforeUnmount() {
+	        delete this._closeButton;
+	    }
+
+	    onCloseModal(){
+	    	if ( event.target.classList.contains('modalSlidder') ) {
+	    	    this.onClose();
+	    	}
+	    }
+
+	    onClose(event) {
+	    	
+	    	
+	    	if ( document.querySelector('.modal') ) {
+	    		document.querySelector('.modal').style.opacity = 1;
+	    	}else{
+	    		let html = document.getElementsByTagName('html')[0]
+	    		html.classList.remove('html_overflow');
+	    		html.style.marginRight = '0px';
+	    	}
+	        this.unmount();
+	    }
+
+
 	}
 
 	return ModalSlider;
