b312f722a2f608bcfaa4fe1265edfa338e9a5933
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-04-25 23:36:35 +0500

    Добавлена глобальная переменная user_id

--- js/app.js
+++ js/app.js
@@ -28,6 +28,6 @@ class AbstractFactory {
 }
 // eslint-disable-next-line no-unused-vars
 const factory = new AbstractFactory();
-let page, modal, modalslider, modalAddPhoto;
+let page, modal, modalslider, modalAddPhoto, user_id;
 
 require(['app/main']);
