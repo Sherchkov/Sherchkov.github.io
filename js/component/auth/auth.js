ba43c0bd2023f2aaf3399b1cfb257d32a86ff96a
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-04-25 23:35:30 +0500

    Добавлена регистрация

--- js/component/auth/auth.js
+++ js/component/auth/auth.js
@@ -30,25 +30,25 @@ define(['base/component', 'css!component/auth/auth'], function (Component) {
                             <div class="join-formReg__error"></div>
                             <div class="join-form__container">
                                 <span class="join-form__title">Логин<sup class="join-form__sub">*</sup></span>
-                                <input class="join-form__input join-form__input_logReg" type="text">
+                                <input class="join-form__input join-form__input_logReg" type="text" value="Login">
                                 <span class="join-form__title">Пароль<sup class="join-form__sub">*</sup></span>
-                                <input class="join-form__input join-form__input_passReg" type="password">
+                                <input class="join-form__input join-form__input_passReg" type="password" value="password">
                                 <span class="join-form__title">Повторите пароль<sup class="join-form__sub">*</sup></span>
-                                <input class="join-form__input join-form__input_passRecReg" type="password">
+                                <input class="join-form__input join-form__input_passRepReg" type="password" value="password">
                                 <span class="join-form__title">Имя</span>
-                                <input class="join-form__input join-form__input_nameReg" type="text">
+                                <input class="join-form__input join-form__input_nameReg" type="text" value="Имя">
                                 <span class="join-form__title">Фамиллия</span>
-                                <input class="join-form__input join-form__input_familyReg" type="text">
+                                <input class="join-form__input join-form__input_familyReg" type="text" value="Фамилия">
                                 <span class="join-form__title">Город</span>
-                                <input class="join-form__input join-form__input_cityReg" type="text">
+                                <input class="join-form__input join-form__input_cityReg" type="text" value="Уфа">
                                 <span class="join-form__title">Дата рождения</span>
-                                <input class="join-form__input join-form__input_birthReg" type="date">
+                                <input class="join-form__input join-form__input_birthReg" type="date" value="1997-03-16">
                                 <span class="join-form__title">Образование</span>
-                                <input class="join-form__input join-form__input_educationReg" type="text">
+                                <input class="join-form__input join-form__input_educationReg" type="text" value="УГНТУ">
                                 <span class="join-form__title">Работа</span>
-                                <input class="join-form__input join-form__input_jobReg" type="text">
+                                <input class="join-form__input join-form__input_jobReg" type="text" value="Работа мечты">
                                 <span class="join-form__title">Семейное положение</span>
-                                <input class="join-form__input join-form__input_stateReg" type="text">
+                                <input class="join-form__input join-form__input_stateReg" type="text" value="Холост">
                                 <button class="join-form__send join-form__send_reg">Зарегистрироваться</button>
                             </div>  
                         </div>
@@ -68,7 +68,7 @@ define(['base/component', 'css!component/auth/auth'], function (Component) {
             this.subscribeTo(this._enterTitle, 'click', this.ShowFormEnter.bind(this));
             this.subscribeTo(this._regTitle, 'click', this.ShowFormReg.bind(this));
             this.subscribeTo(this._buttonEnter, 'click', this.Enter.bind(this));
-            /*this.subscribeTo(this._buttonReg, 'click', this.Registration.bind(this));*/
+            this.subscribeTo(this._buttonReg, 'click', this.Registration.bind(this));
         }
 
         ShowFormEnter(){
@@ -84,8 +84,6 @@ define(['base/component', 'css!component/auth/auth'], function (Component) {
           this._formEnter.classList.add('join-form_none');
           this._formReg.classList.remove('join-form_none');
         }
-
-
 
         Enter(){
             let login = document.querySelector('.join-form__input_logEneter').value,
@@ -124,7 +122,7 @@ define(['base/component', 'css!component/auth/auth'], function (Component) {
                     }
                 })
                 .then(result => {
-                    console.log("result", result);
+                    user_id = result.id;
                     page.unmount();
                     if (window.innerWidth > 800) {
                         require(["page/profile"], function (Profile) {
@@ -141,6 +139,85 @@ define(['base/component', 'css!component/auth/auth'], function (Component) {
                 .catch(error => console.log('error', error));
         }
 
+        Registration(){
+            let login = document.querySelector('.join-form__input_logReg').value,
+                password = document.querySelector('.join-form__input_passReg').value,
+                repeat_password = document.querySelector('.join-form__input_passRepReg').value,  
+                errorEnter = document.querySelector('.join-formReg__error');
+
+            errorEnter.innerText = '';
+
+            if (!this.check(login, 'login', errorEnter) || !this.check(password, 'password', errorEnter)) {
+                window.scrollTo(pageXOffset, 0);
+                return;
+            }
+        
+            if (password !== repeat_password) {
+                errorEnter.innerText = 'Ошибка: Пароли не совпадают';
+                window.scrollTo(pageXOffset, 0);
+                return false;
+            }
+
+            let data = {
+              data : {
+                name : `${document.querySelector('.join-form__input_nameReg').value + ' ' + document.querySelector('.join-form__input_familyReg').value}` || 'Скрыто',
+                birth_date : document.querySelector('.join-form__input_birthReg').value,
+                city : document.querySelector('.join-form__input_cityReg').value || 'Скрыто',
+                family_state : document.querySelector('.join-form__input_stateReg').value || 'Скрыто',
+                education : document.querySelector('.join-form__input_educationReg').value || 'Скрыто',
+                job : document.querySelector('.join-form__input_jobReg').value || 'Скрыто',
+              }
+            }
+
+
+            let urlencoded = new URLSearchParams();
+            urlencoded.append('login', login);
+            urlencoded.append('password', password);
+
+            fetch('https://tensor-school.herokuapp.com/user/create', {
+                method: 'POST',
+                headers: {"Content-Type": "application/x-www-form-urlencoded"},
+                body: urlencoded,
+                "credentials" : "include"
+            })
+            .then(response => {
+                if ( response.ok ) {
+                    return response.json();
+                }else{
+                    console.log("response", response);  
+                    if (response.status === 400) {
+                        window.scrollTo(pageXOffset, 0);
+                        errorEnter.innerText = 'Ошибка: Логин или пароль введены не правильно или такой login уже зарегистрирован';
+                    }else{
+                        errorEnter.innerText = 'Ошибка сервера';
+                    }
+                    return response.error();
+                }      
+            })
+            .then(result => {
+                user_id = result.id;
+                setTimeout(this.updateUser(data),3000);
+            })
+            .catch(error => console.log('error', error));
+        }   
+
+
+        updateUser(data){
+            console.log("data", data);
+            fetch('https://tensor-school.herokuapp.com/user/update', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body : JSON.stringify(data),
+                credentials: 'include'
+            })
+            .then(response => {
+                this.loadpage();
+            })
+            .catch(error => {
+                this.loadpage();
+                console.log('error', error);
+            });  
+        }
 
         check(input, parametr,errorEnter){
             if ( parametr === 'login') {
@@ -168,6 +245,22 @@ define(['base/component', 'css!component/auth/auth'], function (Component) {
 
             return true;
         }
+
+        loadpage(){
+            page.unmount();
+            if ( window.innerWidth > 800 ) {
+                require(["page/profile"], function (Profile) {
+                    page = factory.create(Profile, {});
+                    page.mount(document.body);
+                });
+            } else {
+                require(["page/ProfileMobile"], function(profileMobile){
+                    page = factory.create(profileMobile, {});
+                    page.mount(document.body);
+                });
+            }
+        }
+
     }
     return Auth;
 });
