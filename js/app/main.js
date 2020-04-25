1895c630658ac9728ea11a289c88ee8a2e60c970
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-04-25 23:39:05 +0500

    Присваивание user_id

--- js/app/main.js
+++ js/app/main.js
@@ -1,24 +1,27 @@
 fetch("https://tensor-school.herokuapp.com/user/current", {
 	credentials: 'include'
 })
-.then(response => response.status)
-.then(status => {
-	if (status === 200) {
-		if ( window.innerWidth > 800 ) {
-			require(["page/profile"], function (Profile) {
-				page = factory.create(Profile, {});
-				page.mount(document.body);
-			});
-		} else {
-			require(["page/ProfileMobile"], function(profileMobile){
-				page = factory.create(profileMobile, {});
-				page.mount(document.body);
-			});
-		}
-	}
-	else {
+.then(response => {
+	if ( response.ok ) {
+	    return response.json();  
+	}else{
 		require(["page/Authorization"], function(Authorization){
 			page = factory.create(Authorization, {});
+			page.mount(document.body);
+		});
+	  	return response.error();
+	}
+})
+.then(result => {
+	user_id = result.id;
+	if ( window.innerWidth > 800 ) {
+		require(["page/profile"], function (Profile) {
+			page = factory.create(Profile, {});
+			page.mount(document.body);
+		});
+	} else {
+		require(["page/ProfileMobile"], function(profileMobile){
+			page = factory.create(profileMobile, {});
 			page.mount(document.body);
 		});
 	}
