b2b44a97eba3d2b6597d6c93d3f89178d2f8f877
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-05-05 12:17:32 +0500

    Update slider.js
    Добавлено поведение при удалении или добавлении фотографии в галерею

--- js/component/action/slider.js
+++ js/component/action/slider.js
@@ -1,6 +1,7 @@
 define(function(){
 	function slider(element, config = {}){
 
+		// element - Обертка слайдера (Пример content-gallery)
 		// elementContainer - контейнер изображений
 		// widthContainer - ширина контейнера изображений
 		// elementItems - блок с изображением 
@@ -13,66 +14,156 @@ define(function(){
 		// transform - значение трансфомации для контейнера
 		// arrowLeft - кнопка назад
 		// arrowRight - кнопка вперед
-
-		let elementContainer = element.querySelector('.slider-container'),
-			widthContainer = 0,
-			elementItems = element.querySelectorAll('.slider-container__item'),
-			widthItem = 0,
-			amountElementItems = elementItems.length,
-			items = config.items || [],
-			itemsLenght = 0,
+		let elementContainer = element.querySelector('.slider-container'), //используется 
+			view = element.querySelector('.slider__view'), // используется
+			widthContainer = 0, // используется
+			elementItems = element.querySelectorAll('.slider-container__item'), // используется
+			bufer = element.querySelector('.slider__bufer'),
+			widthItem = 0, // используется
+			globalItems = config.globalItems || false, //используется
+			items = config.items || globalSliderPhotos, // используется
 			step = 0,
-			dstep = config.dstep || 1,
-			position = config.position || 0,
-			transform = 0,
-			classItem = config.classItem || 'slider-container__item_modal',
-			classImg = config.classImg || 'slider-container__img_max',
-			arrowLeft = config.leftButton || element.querySelector('.slider-arrowLeft'),
-			arrowRight = config.RightButton || element.querySelector('.slider-arrowRight');
-
-		// если нету переданных изображений, то выходим
-		if ( !items.length && !amountElementItems ){
-			return false;
-		}
-
-		// если элементы не переданы через config , то заносим их в items  
-		if ( !items.length ) {
-			elementItems.forEach(function(elementItem){
-				items.push(elementItem);
-			});
-		}
-		//определяем количество элементов для отображения
-		itemsLenght = items.length;
+			dstep = config.dstep || 1, // НЕ используется
+			position = config.position || 0, //используется 
+			dataId = config.dataId || 0, //используется 
+			transform = 0, //используется 
+			qt = 0, //используется 
+			classItem = config.classItem || 'slider-container__item_modal', //используется 
+			classImg = config.classImg || 'slider-container__img_max', //используется 
+			arrowLeft = config.leftButton || element.querySelector('.slider-arrowLeft'), //используется 
+			arrowRight = config.RightButton || element.querySelector('.slider-arrowRight'); //используется 
+ 
+		
+		
 
 		// Инициализация слайдера
 		function initialization(){
-			// обновляем параметры
-			updateStep();
-			// получаем максимальное количество видимых элементов
-			let qt = getCountVisible();
-			// если максимальное количество видимых элементов больше, чем количество всех элементов, то прерываем инициализацию
-			if ( qt >= itemsLenght ) {
-				return;
+			if (items.length) {
+				update();
+				if (items.length > qt) {
+					if ( position === 0 ) {
+						arrowRight.style.display = 'block';
+					}else if ( position >=  items.length - qt  ){
+						arrowLeft.style.display = 'block';
+					}else{
+						arrowLeft.style.display = 'block';
+						arrowRight.style.display = 'block';
+					}
+				}
 			}
-			//если позиция 0, то добавляем кнопку только вперед; если позиция последнего элемента, то добавляем кнопку назад; иначе добавляем кнопку вперед и назад;
-			if ( position === 0 ) {
-				arrowRight.style.display = 'block';
-			}else if ( position >=  itemsLenght - qt  ){
-				arrowLeft.style.display = 'block';
-			}else{
-				arrowLeft.style.display = 'block';
-				arrowRight.style.display = 'block';
-			}
-			// добавление обработчиков
 			setEventListener();
 		}
 
-		/**
-		   * Возвращает true если элемент c data-position есть, иначе false
-		   * @param {position} - Позиция отображаемого элемента
-		  */
-		function searchSlide(position){
-			let slide = element.querySelector('.slider-container__item[data-position="'+ position +'"]');
+		// Обновление слайдера при удалении фотографий
+		function fullupdate(){
+			items = globalSliderPhotos;
+			qt = getCountVisible();
+			step = widthItem / widthContainer * 100;
+			position = 0;
+			for (let i = 0; i < elementItems.length; i++){
+				// Math.abs(elementItems[i].getBoundingClientRect().left - view.getBoundingClientRect().left) < elementItems[i].getBoundingClientRect().width
+				if ( elementItems[i].getBoundingClientRect().left - view.getBoundingClientRect().left > -10 ) {
+					dataId = elementItems[i].getAttribute('data-id');
+					break;
+				}
+			}
+
+			for (let i = 0; i <= items.length; i++){
+				if (items[i].id === Number(dataId)) {
+					break;
+				}
+				position++;
+			}
+
+			
+			bufer.innerHTML = '';
+			transform = elementContainer.style.transform || elementContainer.style.webkitTransform || elementContainer.style.mozTransform || 'translateX(0%)';
+			transform = parseFloat(transform.substr(11).slice(0, -2));
+		}
+
+		// Обновление слайдера
+		function update(){
+			//Если были добавлены новые фотографии
+			if (globalItems && bufer && bufer.innerHTML === 'add') {
+				updateSize();
+			}
+			//Если были удалены какие-то фотографии
+			if (globalItems && bufer && bufer.innerHTML === 'delete') {
+				updateSize();
+				fullupdate();
+				return;
+			}
+			qt = getCountVisible();
+			step = widthItem / widthContainer * 100;
+		}
+
+		//Обновляет размеры
+		function updateSize(){
+			elementContainer = element.querySelector('.slider-container');
+			elementItems = element.querySelectorAll('.slider-container__item');
+			bufer.innerHTML = '';
+		}
+
+
+		function nextSlide(){
+			update();
+			if ( items[position +qt] ) {
+				if ( !searchElementOnPage(items[position + qt].id) ) {
+					let newElement = createElement(items[position + qt]);
+					elementItems = element.querySelectorAll('.slider-container__item');
+					elementContainer.append(newElement);
+				}
+				
+				transform -= step;
+				elementContainer.style.transform = 'translateX(' + transform + '%)';
+				arrowLeft.style.display = 'block';
+				position++;
+				if (position + qt === items.length) {
+					arrowRight.style.display = 'none';
+				}
+			}
+			else{
+				console.log('элемент не найден, что очень странно!)');
+				reload();
+			}
+		}
+
+		//Предыдущий слайд
+		function prevSlide(){
+			update();
+			if ( items[position - 1] ) {
+				if ( !searchElementOnPage(items[position - 1].id) ) {
+					let newElement = createElement(items[position - 1]);
+					elementItems = element.querySelectorAll('.slider-container__item');
+					elementContainer.prepend(newElement);	
+				}else{
+					transform += step;
+					elementContainer.style.transform = 'translateX(' + transform + '%)';
+				}
+				arrowRight.style.display = 'block';
+				
+				position--; 
+				if (position === 0) {
+					arrowLeft.style.display = 'none';
+				}
+			}
+			else{
+				console.log('элемент не найден, что очень странно!)');
+				reload();
+			}
+		}
+
+		//Создает фотографию 
+		function createElement(data){
+			let slideHTML = document.createElement('div');
+			slideHTML.className = 'slider-container__item ' + classItem;
+			slideHTML.setAttribute('data-id', data.id);
+			slideHTML.innerHTML = `<img src="${globalUrlServer+data.path}" alt="фотография" class="slider-container__img ${classImg}">`;
+			return slideHTML;
+		}
+
+		function searchElementOnPage(idElement){
+			let slide = element.querySelector('.slider-container__item[data-id="'+ idElement +'"]');
 			if ( slide ) {
 				return true;
 			}else{
@@ -80,130 +171,48 @@ define(function(){
 			}
 		}
 
-		/**
-		   * Добавление нового блока с изображением в контейнер
-		   * @param {direction} - Направление в начало контейнера или в конец
-		   * @param {position} - Позиция нового элемента
-		  */
-		function addSlide(direction,position){
-
-			if (amountElementItems === itemsLenght) {
-				return;
+		//Перезагружает слайдер, если возникла ошибка
+		function reload(){
+			let sliderContainer = '';
+			for (let i = 0; i < items.length; i++){
+				if (i === qt || i >= 4) {
+					break;
+				}
+			    sliderContainer += `
+			        <div class="slider-container__item ${classItem}" data-id="${items[i].id}">
+			          <img src="${globalUrlServer+ items[i].path}" alt="фотография" class="slider-container__img ${classImg}">
+			        </div>`;
 			}
-			let qt = getCountVisible();
-			console.log("qt", qt);
-
-			if (classItem === 'slider-container__item_gallery') {
-				qt = 3;
-			}else{
-				qt -= 1;
-			}
-
-			let slideHTML = document.createElement('div');
-			slideHTML.className = 'slider-container__item ' + classItem;
-			slideHTML.setAttribute('data-position', position + qt);
-			slideHTML.innerHTML = `<img src="${items[position + qt]}" alt="фотография" class="slider-container__img ${classImg}">`;
-
-			if (direction === 'prev') {
-				elementContainer.prepend(slideHTML);
-			}
-			if (direction === 'next') {
-				elementContainer.append(slideHTML);
-				elementContainer.style.transform = 'translateX(' + (transform - step) + '%)';
-			}
-
+			transform = 0;
+			position = 0;
+			arrowLeft.style.display = 'none';
+			arrowRight.style.display = 'none';
+			elementContainer.style.transform = 'translateX(' + transform + '%)';
+			elementContainer.innerHTML = sliderContainer;
+			elementItems = element.querySelectorAll('.slider-container__item');
+			initialization();
 		}
 
 		/**
-		   * Возвращает максимальное количество видимых элементов
-		  */
+			* Добавление обработчиков на кнопку назад и вперед
+		*/
+		function setEventListener(){
+			arrowLeft.addEventListener('click', prevSlide);	
+			arrowRight.addEventListener('click', nextSlide);
+		}
+
+		/**
+		   *Возвращает максимальное количество видимых элементов
+		*/
 		function getCountVisible(){
+			widthContainer = parseFloat(getComputedStyle(elementContainer).width);
+			console.log("widthContainer", widthContainer);
+			widthItem = parseFloat(getComputedStyle(elementItems[0]).width);
+			console.log("widthItem", widthItem);
+			console.log("elementItems[0]", elementItems[0]);
 			return Math.round(widthContainer / widthItem);
 		}
-
-		/**
-		   * Добавление обработчиков на кнопку назад и вперед
-		  */
-		function setEventListener(){
-			arrowLeft.addEventListener('click', clickСontroller);	
-			arrowRight.addEventListener('click', clickСontroller);
-		}
-			
-		/**
-		   * Обновление размеров элементов (при изменении экрана)
-		  */
-		function updateStep(){
-			widthContainer = parseFloat(getComputedStyle(elementContainer).width);
-			widthItem = parseFloat(getComputedStyle(elementItems[0]).width);
-			step = widthItem / widthContainer * 100;
-			if (itemsLenght != amountElementItems) {
-				amountElementItems = element.querySelectorAll('.slider-container__item').length;
-			}
-		}
 		
-		/**
-		   * Определение события prev/next
-		  */
-		function clickСontroller(event){
-			if ( this === arrowLeft ) {
-				switchSlide('prev');
-			}else if ( this === arrowRight ) {
-				switchSlide('next');
-			}else{
-				return;
-			}
-		}
-
-		/**
-		   * Переключение слайда
-		   * @param {direction} - Направление вперед или назад
-		  */
-		function switchSlide(direction){
-			updateStep();
-			let qt = getCountVisible();
-			if (direction === 'prev') {
-				if (position - 1 < 0) {
-					return;
-				}
-				if (position - 1 == 0) {
-					arrowLeft.style.display = 'none';
-				}
-
-				arrowRight.style.display = 'block';
-				position--;
-				if (amountElementItems !== itemsLenght && position >= 0 && !searchSlide(position) ) {
-					addSlide('prev', position);
-					return;
-				}
-
-			  	transform += step;
-			}
-
-			if (direction === 'next') {
-				if (position + qt + 1 > items.length) {
-					return;
-				}
-				if (position + qt + 1 == items.length) {
-					arrowRight.style.display = 'none';
-				}
-
-				arrowLeft.style.display = 'block';
-				position++;
-
-				if (amountElementItems !== itemsLenght && position + qt - 1< items.length ) {
-					console.log("items.length", items.length);
-					console.log("position", position);
-					console.log(position + qt - 1)
-					addSlide('next', position)
-				}
-
-			  	transform -= step;
-			  	
-			}
-
-			elementContainer.style.transform = 'translateX(' + transform + '%)';
-		}
-
 		initialization();
 	}	
 	return slider;
