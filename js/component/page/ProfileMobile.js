dc761247e475184e6a5848a834ae9c2b886ae328
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-05-06 22:47:52 +0500

    Добавлена передача данных к аватар
    Добавлена передача данных к компоненту аватар для мобильной версии

--- js/component/page/ProfileMobile.js
+++ js/component/page/ProfileMobile.js
@@ -1,18 +1,19 @@
 define(['base/component', 'component/header/header', 'component/information/informationMobile', 'component/gallery/gallery', 'component/wall/wall', 'component/navigation/navigation', 'component/messages/messages'], function(Component, Header, InformationMobile, Gallery, Wall, Navigation, Messages){
 	class Profile extends Component {
-	    render() {
+	    render(options) {
+	    	options.mobile = true;
 	        return `
 	            <div class="MainPage MainPageMobile">
 	                <!-- header -->
 	                ${this.childrens.create(Header, {})}
                 	<!-- Блок с данными о пользователе -->
-                	${this.childrens.create(InformationMobile, {mobile:true})}
+                	${this.childrens.create(InformationMobile, options)}
                 	<!-- Навигация -->
-					${this.childrens.create(Navigation, {})}
+			${this.childrens.create(Navigation, {})}
                 	<!-- Галлерея -->
                 	${this.childrens.create(Gallery, {})}
                 	<!-- Сообщения -->
-					${this.childrens.create(Messages, {mobile:true})}
+			${this.childrens.create(Messages, {mobile:true})}
                 	<!-- Стена -->
                  	${this.childrens.create(Wall, {})}
 	            </div>
