e405dab8a1a3709cf11338527403171f4a4b89d8
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-05-06 22:17:37 +0500

    Передача данных о пользователе к компоненту аватар

--- js/component/page/profile.js
+++ js/component/page/profile.js
@@ -1,6 +1,6 @@
 define(['base/component', 'component/header/header', 'component/information/information', 'component/gallery/gallery', 'component/wall/wall', 'component/avatar/avatar', 'component/navigation/navigation', 'component/messages/messages'], function(Component, Header, Information, Gallery, Wall, Avatar, Navigation, Messages){
 	class Profile extends Component {
-	    render() {
+	    render(options) {
 	        return `
 	            <div class="MainPage">
 	                <!-- header -->
@@ -16,7 +16,7 @@ define(['base/component', 'component/header/header', 'component/information/info
 	                    </div>
 	                    <div class="Content__side-bar side-bar">
 	                     	<!-- Профиль -->
-	                     	${this.childrens.create(Avatar, {})}
+	                     	${this.childrens.create(Avatar, options)}
 	                     	<!-- Навигация -->
 	                     	${this.childrens.create(Navigation, {})}
 	                     	<!-- Сообщения -->
