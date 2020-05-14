6ca6bee1c3abbc72911c84c4f8125a311eceabb7
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-05-14 15:30:04 +0500

    Добавлено окно предупреждения
    Добавлено окно предупреждения для какого-нибудь действия (удаление фотографии)

---
+++ js/component/modal/ModalWarning.js
@@ -0,0 +1,70 @@
+define(['base/component'], function (Component) {
+	'use strict';
+
+	class ModalWarning extends Component{
+	    render(options) {
+	    	this.options = options;
+	        return ` 
+	            <div class="warning">
+	            	<div class="warning__title">Предупреждение</div>
+	            	<div class="warning-content">
+						${this.options.text}
+	            	</div>
+	            	<div class="warning-footer">
+	            		<button class="warning__cansel">Отмена</button>
+	            		<button class="warning__act" data-act="${this.options.dataButton}">${this.options.textButton}</button>
+	            	</div>
+	            </div>
+	        `;
+	    }
+
+	    afterMount() {
+	        this._closeButton = this.getContainer().querySelector('.warning__cansel');
+	        this.subscribeTo(this._closeButton, 'click', this.onClose.bind(this));
+	        this._act = this.getContainer().querySelector('.warning__act');
+	        this.subscribeTo(this._act, 'click', this.onClick.bind(this));
+	    }
+
+	    onClick(){
+	    	let data = this.getContainer().querySelector('.warning__act').getAttribute('data-act');
+	    	if (data === 'deletePhoto') {
+	    		this.deleteAvatar();
+	    	}
+	    	this.onClose();
+	    }
+	    
+	    deleteAvatar(){
+	    	//Удаляем фотографию
+	    	fetch(globalUrlServer + '/user/upload_photo', {
+	    		method : 'POST',
+	    		headers: {"Content-Type": "image/png"},
+	    		body: null,
+	    		credentials: 'include'  
+	    	})
+	    	.then(response => {
+	    		if ( response.ok ) {
+	    			document.querySelector('.content-photo .content-photo__img').setAttribute('src', 'img/avatar/avatar_default.png');
+	    			document.querySelector('.header .header__img').setAttribute('src', 'img/avatar/avatar_default.png');
+	    			document.querySelector('.content-photo .photo__delete').remove();
+	    		}else{
+	    			alert('Не удалось удалить фотографию');
+	    			return;
+	    		}  
+	    	  
+	    	})
+	    	.catch(error => {
+				alert('Не удалось удалить фотографию');
+	    		console.log('error', error);
+	    	});
+	    }
+
+	    onClose(event) {
+	    	let html = document.getElementsByTagName('html')[0];
+	    	html.classList.remove('html_overflow');
+	    	html.style.marginRight = '0px';
+	        modal.unmount();
+	    }
+	}
+
+	return ModalWarning;
+});  
