066a4d45444a944592642a7b0e9acf304a2ccf10
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-05-23 04:31:13 +0500

    Выгрузка плеера в header

--- js/component/action/music.js
+++ js/component/action/music.js
@@ -11,6 +11,16 @@ define (['jquery'], function(){
             $('.playlist li').removeClass('playlist__current-song');
             currentSong = $(this).parent().index();
             $(this).parent().addClass('playlist__current-song');
+            if (!musicHeader) {
+                require(['Component/action/player', 'server/json', 'Component/player/player'], function(AudioPlayer, json){ 
+                    musicHeader = new AudioPlayer(json.music, document.querySelector('#audioHeader'))
+                    musicHeader.setPosition(currentSong);
+                    musicHeader.setStartTitle();
+                });
+            }else{
+                musicHeader.setPosition(currentSong);
+                musicHeader.setStartTitle();
+            }
         });
 
         $('.audioPlayer')[0].addEventListener('ended', function () {
@@ -22,6 +32,8 @@ define (['jquery'], function(){
             $('.playlist li:eq(' + currentSong + ')').addClass('playlist__current-song');
             $('.audioPlayer')[0].src = $('.playlist li a')[currentSong].href;
             $('.audioPlayer')[0].play();
+            musicHeader.setPosition(currentSong);
+            musicHeader.setStartTitle();
         });
     }
     
