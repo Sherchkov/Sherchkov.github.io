b791d0ebd740ac1bc931786bf71d5d90c4ec3e2d
Ramil-ufa <64061348+Ramil-ufa@users.noreply.github.com>
2020-05-25 00:21:17 +0500

    Сохранение темы и позиции при переходе на другую

--- js/component/page/profile.js
+++ js/component/page/profile.js
@@ -3,16 +3,20 @@ define(['base/component', 'component/header/header', 'component/information/info
 	    render(options) {
 	    	this.options = options;
 	    	document.title = options.data.name;
+	    	if (this.options.id === user_id) {
+	    		userParams.theme_night = this.options.data.theme_night || 'false';
+	    		userParams.mirror = this.options.data.mirror || 'false';
+	    	}
 	        return `
 	            <div class="MainPage">
 	                <!-- header -->
 	                ${this.childrens.create(Header, options)}
-	                <div class="MainPage__Content Content ${options.data.mirror === 'true' ? 'content_left' : ''}">
+	                <div class="MainPage__Content Content ${(userParams.mirror === 'true' || (user_id === this.options.id && options.data.mirror === 'true') ) ? 'content_left' : ''}">
 	                    <div class="Content__body con-body">
 	                     	<!-- Блок с данными о пользователе -->
 	                     	${this.childrens.create(Information, options)}
 	                     	<!-- Галлерея -->
-	                     	${this.childrens.create(Gallery, {})}
+	                     	${this.childrens.create(Gallery, options)}
 	                     	<!-- Стена -->
 	                      	${this.childrens.create(Wall, options)}
 	                    </div>
@@ -22,7 +26,7 @@ define(['base/component', 'component/header/header', 'component/information/info
 	                     	<!-- Навигация -->
 	                     	${this.childrens.create(Navigation, options)}
 	                     	<!-- Сообщения -->
-				<!--${this.childrens.create(Messages, {})}-->
+							<!--${this.childrens.create(Messages, {})}-->
 	                    </div>
 	                </div>
 	            </div>
@@ -30,12 +34,13 @@ define(['base/component', 'component/header/header', 'component/information/info
 	    }
 
 	    afterMount(){
-	    	if (this.options.data.theme_night === 'true') {
+	    	if (userParams.theme_night === 'true' || (user_id === this.options.id && this.options.data.theme_night === 'true')) {
 	    		document.querySelector('body').style.background = '#35363a';
 	    		document.querySelectorAll('.content_default').forEach(block => {
 	    			block.style.background = '#595a5c';
 	    			block.style.color = '#bfbfbf';
 	    		});   
+	    		document.querySelector('.content-data__name').style.color = '#bfbfbf'; 
 	    		document.querySelector('.content_data__aboutMe').style.color = '#bfbfbf';
 	    		document.querySelector('.content-data-params__date').classList.add('content-data-params__date_night');
 	    		document.querySelectorAll('.content-data-params__input').forEach(block => block.style.color = '#bfbfbf');   
